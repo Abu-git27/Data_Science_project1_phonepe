c3de1e7521cef555a7cb20a9d1ff38a4244b0401
Abu shakeer <abushakeer2002@gmail.com>
2025-07-31 17:17:31 +0530

    Add agg_transaction.py

---
+++ agg_transaction.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import json
+import os
+
+path = 'data/aggregated/transaction/country/india/state/'
+state_list = os.listdir(path)
+clm = {
+    'State': [], 
+    'Year': [], 
+    'Quarter': [],
+    'Transaction_type': [], 
+    'Transaction_count': [], 
+    'Transaction_amount': []
+}
+
+for state in state_list:
+    state_path = os.path.join(path, state)
+    for year in os.listdir(state_path):
+        year_path = os.path.join(state_path, year)
+        for file in os.listdir(year_path):
+            file_path = os.path.join(year_path, file)
+            with open(file_path, 'r') as f:
+                data = json.load(f)
+                if 'data' in data and 'transactionData' in data['data']:
+                    for record in data['data']['transactionData']:
+                        clm['State'].append(state)
+                        clm['Year'].append(year)
+                        clm['Quarter'].append(int(file.strip('.json')))
+                        clm['Transaction_type'].append(record['name'])
+                        clm['Transaction_count'].append(record['paymentInstruments'][0]['count'])
+                        clm['Transaction_amount'].append(record['paymentInstruments'][0]['amount'])
+
+Agg_Transaction = pd.DataFrame(clm)
+print(Agg_Transaction)
+Agg_Transaction.to_csv('agg_transaction.csv', index=False)
